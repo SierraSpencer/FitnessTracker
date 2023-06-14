c9a328f81551ec1e4d3767768a1b046b5ab842b3
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-13 18:12:22 -0700

    Update routine_activities.js
    mine

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -5,11 +5,11 @@ async function addActivityToRoutine({
   activityId,
   count,
   duration,
-}) {
-  try {
-    const {
-      rows: [routineAct],
-    } = await client.query(
+}) 
+{
+  try 
+  {
+    const {rows} = await client.query(
       `
       INSERT INTO routine_activities ("routineId", "activityId", count, duration)
       VALUES ($1, $2, $3, $4)
@@ -17,18 +17,19 @@ async function addActivityToRoutine({
     `,
       [routineId, activityId, count, duration]
     );
-
-    return routineAct;
-  } catch (error) {
-    throw error;
+    const [routineActivity] = rows;
+    return routineActivity;
+  } catch (error) 
+  {
+    console.error(error);
   }
 }
 
-async function getRoutineActivityById(id) {
-  try {
-    const {
-      rows: [rouAct],
-    } = await client.query(
+async function getRoutineActivityById(id) 
+{
+  try 
+  {
+    const {rows} = await client.query(
       `
       SELECT *
       FROM routine_activities
@@ -36,44 +37,41 @@ async function getRoutineActivityById(id) {
     `,
       [id]
     );
-
-    return rouAct;
+    const [routineActivity] = rows;
+    return routineActivity;
   } catch (error) {
-    throw error;
+    console.error(error);
   }
 }
 
-async function getRoutineActivitiesByRoutine({ id }) {
-  try {
-    const { rows: rouAct } = await client.query(
+async function getRoutineActivitiesByRoutine({ id }) 
+{
+  try 
+  {
+    const {rows} = await client.query(
       `
       SELECT routine_activities.*, activities.name, activities.description
       FROM activities
-      JOIN routine_activities ON "activityId"=activities.id
+      JOIN routine_activities ON routine_activities."activityId"=activities.id
       WHERE "routineId"=$1;
     `,
       [id]
     );
-
-    return rouAct;
-  } catch (error) {
-    throw error;
+    rreturn rows;
+  } catch (error) 
+  {
+    console.error(error);
   }
 }
 
-async function updateRoutineActivity({ id, ...fields }) {
+async function updateRoutineActivity({ id, ...fields }) 
+{
   const setString = Object.keys(fields)
     .map((key, index) => `"${key}"=$${index + 1}`)
     .join(", ");
-
-  if (setString.length === 0) {
-    return;
-  }
-
-  try {
-    const {
-      rows: [rouAct],
-    } = await client.query(
+  try 
+  {
+    const {rows} = await client.query(
       `
       UPDATE routine_activities
       SET ${setString}
@@ -82,18 +80,19 @@ async function updateRoutineActivity({ id, ...fields }) {
     `,
       Object.values(fields)
     );
-
-    return rouAct;
-  } catch (error) {
-    throw error;
+    const [routineActivity] = rows;
+    return routineActivity;
+  } catch (error) 
+  {
+    console.error(error);
   }
 }
 
-async function destroyRoutineActivity(id) {
-  try {
-    const {
-      rows: [rouAct],
-    } = await client.query(
+async function destroyRoutineActivity(id) 
+{
+  try 
+  {
+    const {rows} = await client.query(
       `
       DELETE FROM routine_activities
       WHERE id=$1
@@ -101,18 +100,19 @@ async function destroyRoutineActivity(id) {
     `,
       [id]
     );
-
-    return rouAct;
-  } catch (error) {
-    throw error;
+    const [routineActivity] = rows;
+    return routineActivity;
+  } catch (error) 
+  {
+    console.error(error);
   }
 }
 
-async function canEditRoutineActivity(routineActivityId, userId) {
-  try {
-    const {
-      rows: [routine_activities],
-    } = await client.query(
+async function canEditRoutineActivity(routineActivityId, userId) 
+{
+  try 
+  {
+    const {rows,} = await client.query(
       `
     SELECT *
     FROM routine_activities
@@ -121,14 +121,17 @@ async function canEditRoutineActivity(routineActivityId, userId) {
     `,
       [routineActivityId]
     );
-
-    if (routineActivityId === userId) {
+    const [routineActivity] = rows;
+    if (routineActivity.creatorId === userId) 
+    {
       return true;
-    } else {
+    } else 
+    {
       return false;
     }
-  } catch (error) {
-    throw error;
+  } catch (error) 
+  {
+    console.error(error);
   }
 }
 
