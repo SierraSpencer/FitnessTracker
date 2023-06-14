d1e6db22e6332549168e7b534bbc1a999932f14b
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-13 18:22:04 -0700

    Update seedData.js
    mine

--- db/seedData.js
+++ db/seedData.js
@@ -1,72 +1,52 @@
 // require in the database adapter functions as you write them (createUser, createActivity...)
-// const { } = require('./');
 const client = require("./client");
 const { createUser } = require("./users");
 const { createActivity, getAllActivities } = require("./activities");
 const { createRoutine, getRoutinesWithoutActivities } = require("./routines");
 const { addActivityToRoutine } = require("./routine_activities");
 
-async function dropTables() {
-  try {
-    console.log("Dropping All Tables...");
-
-    await client.query(`
-        DROP TABLE IF EXISTS routine_activities;
-        DROP TABLE IF EXISTS routines;
-        DROP TABLE IF EXISTS activities;
-        DROP TABLE IF EXISTS users;
-    `);
-
-    console.log("Dropped");
-  } catch (error) {
-    console.error("I AM THE TABLE!");
-    throw error;
-  }
-
+async function dropTables() 
+{
   // drop all tables, in the correct order
+  console.log("Dropping All Tables...");
+  await client.query(`
+      DROP TABLE IF EXISTS routine_activities;
+      DROP TABLE IF EXISTS routines;
+      DROP TABLE IF EXISTS activities;
+      DROP TABLE IF EXISTS users;
+  `);
 }
 
-async function createTables() {
-  try {
-    console.log("Starting to build tables...");
-
-    await client.query(`
-        CREATE TABLE users (
-          id SERIAL PRIMARY KEY,
-          username VARCHAR(255) UNIQUE NOT NULL,
-          password VARCHAR(255) NOT NULL
-        );
-
-        CREATE TABLE activities (
-          id SERIAL PRIMARY KEY,
-          name VARCHAR(255) UNIQUE NOT NULL,
-          description TEXT NOT NULL
-        );
-
-        CREATE TABLE routines (
-          id SERIAL PRIMARY KEY,
-          "creatorId" INTEGER REFERENCES users(id),
-          "isPublic" BOOLEAN DEFAULT false,
-          name VARCHAR(255) UNIQUE NOT NULL,
-          goal TEXT NOT NULL
-        );
-
-        CREATE TABLE routine_activities (
-          id SERIAL PRIMARY KEY,
-          "routineId" INTEGER REFERENCES routines(id),
-          "activityId" INTEGER REFERENCES activities(id),
-          duration INTEGER,
-          count INTEGER
-        );
-
-    `);
-    console.log("GET THE TABLES!");
-  } catch (error) {
-    console.error("I AM THE TABLE!");
-    throw error;
-  }
-
+async function createTables() 
+{
   // create all tables, in the correct order
+  console.log("Starting to build tables...");
+  await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE activities (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL
+      );
+      CREATE TABLE routines (
+        id SERIAL PRIMARY KEY,
+        "creatorId" INTEGER REFERENCES users(id),
+        "isPublic" BOOLEAN DEFAULT false,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        goal TEXT NOT NULL
+      );
+      CREATE TABLE routine_activities (
+        id SERIAL PRIMARY KEY,
+        "routineId" INTEGER REFERENCES routines(id),
+        "activityId" INTEGER REFERENCES activities(id),
+        duration INTEGER,
+        count INTEGER
+      );
+  `); 
 }
 
 /* 
