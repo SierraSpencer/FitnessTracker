79fc83fb056cdf43df4f120fdf064ded28725428
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-13 18:06:01 -0700

    Update client.js
    mine

--- db/client.js
+++ db/client.js
@@ -1,7 +1,5 @@
 const { Pool } = require("pg");
-const connectionString =
-  process.env.DATABASE_URL ||
-  "https://postgres:postgres@localhost:5432/fitness-dev";
+const connectionString = process.env.DATABASE_URL || "https://postgres:postgres@localhost:5432/fitness-dev";
 const client = new Pool({
   connectionString,
   ssl:
